852f3b9fb6d3c8934ada92052db5eadeee7be6ab
swapnayalamalgandla-spec <swapna.yalamalagandla@example.com>
2026-05-04 11:17:14 +0530

    fix render port crash

--- server.js
+++ server.js
@@ -69,6 +69,8 @@ app.use("/tenants", auth, tenantRoutes);
 app.use("/payments", auth, paymentRoutes);
 
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
